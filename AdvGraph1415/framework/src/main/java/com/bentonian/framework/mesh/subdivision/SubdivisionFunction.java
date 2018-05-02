17aabf39f199fc6395c62824a033668e6f24b475
Jack Deeley <jack.deeley@ntlworld.com>
2018-05-02 09:26:17 +0100

    Removed older examples to save space on my poor dying laptop.

--- AdvGraph1415/framework/src/main/java/com/bentonian/framework/mesh/subdivision/SubdivisionFunction.java
+++
@@ -1,8 +0,0 @@
-package com.bentonian.framework.mesh.subdivision;
-
-import com.bentonian.framework.mesh.Mesh;
-
-public interface SubdivisionFunction {
-
-  public abstract Mesh apply(Mesh mesh);
-}
--- AdvGraph1617/OpenGL Demos/com/bentonian/gldemos/shaders/errata.fsh
+++
@@ -1,8 +0,0 @@
-#version 330
-
-in vec3 c;
-out vec4 fragmentColor;
-
-void main() {
-  fragmentColor = vec4(c, 1);
-}
